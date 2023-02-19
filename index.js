14ceb06f0e3a628fdf49091643d5ef20e4a54c82
ruhul <ra.01403288711@gmail.com>
2023-02-19 19:43:19 +0600

    Controller added for testing API

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
+const testController = require('./controller/testController.js');
+app.use('/test', testController);
+
+app.get('/', function(req, res){
+    res.send("Server is up!");
+ });
+ 
 
 
 app.listen(port,()=>{
